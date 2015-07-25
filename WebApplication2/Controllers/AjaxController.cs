0be1bb8c981cda2f619fec475981406d501a4b92
felixhuang72 <soupo.tw@gmail.com>
2015-07-25 15:02:46 +0800

    練習使用 @Ajax.ActionLink 在 Product.Index() 頁面加上一個 "AJAX-Details" 連結，透過 Ajax 載入 "Details" 頁面的內容 ( 須不含 Layout )

---
+++ WebApplication2/Controllers/AjaxController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getTime()
+        {
+            return Content(DateTime.Now.ToString());
+        }
+    }
+}
